c63c4956ea55063622c3fc7706d5410ccf6427bb
diyeddin <40895033+diyeddin@users.noreply.github.com>
2024-02-19 21:17:26 +0300

    Update app.py

--- app.py
+++ app.py
@@ -29,61 +29,23 @@ class App:
 
 
     def run(self):
+        # wait until user loads a dataset
         try:
             self.load_dataset()
         except:
-            st.write("Please Select File to Continue...")
+            st.write("Please Select a File to Continue...")
             return
         self.preprocess()
         self.train()
 
 
-    def draw_corr(self):
-        st.write("### Correlation Matrix")
-        sns.set_theme(style="white")
-        corr = self.dataset.corr()
-        mask = np.triu(np.ones_like(corr, dtype=bool))
-        f, ax = plt.subplots(figsize=(15, 15))
-        cmap = sns.diverging_palette(230, 20, as_cmap=True)
-        corr_matrix_fig = sns.heatmap(corr, cmap=cmap, mask=mask, vmin=corr.values.min(), vmax=1, center=0, annot=True,
-                                square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot_kws={"size": 6.5})
+    def init_page(self):
+        st.title('YZUP Project - Diyeddin Almohamad')
+        st.write('## Explore Different Classifiers')
 
-        return corr_matrix_fig
-        
-
-    def scatter_plt(self):
-        st.write("### Scatter Plot")
-        plt.figure(figsize=(5,5))
-        scatter = sns.scatterplot(self.dataset, x='radius_mean', y='texture_mean', hue='diagnosis', palette={1:'red', 0:'green'}, alpha=.5)
-        plt.legend(labels=['Malignant', 'Benign'])
-
-        return scatter
-
-
-    def preprocess(self):
-        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)
-        estimator = SVC(kernel="linear")
-        selector = RFE(estimator, n_features_to_select=self.n_features, step=1)
-        selector = selector.fit(self.X_train, self.y_train)
-        self.selected_features = self.X.columns[selector.support_].values
-        st.write("### Selected Features (After RFE)")
-        st.write(self.selected_features)
-
-
-    def gridsearch(self):
-        models = {
-            'KNN': (KNeighborsClassifier(), {'classifier__n_neighbors': (3, 5, 7, 9), 'classifier__leaf_size': (20,40,1), 'classifier__p': (1,2), 'classifier__weights': ('uniform', 'distance'), 'classifier__metric': ('minkowski', 'chebyshev'),}),
-            'SVM': (SVC(), {'classifier__C': (0.1, 1, 10, 15), 'classifier__kernel': ('linear', 'poly', 'rbf', 'sigmoid'), 'classifier__coef0': (0.0, 10.0, 1.0), 'classifier__shrinking': (True, False)})
-        }
-        pipeline = Pipeline([
-            ('scaler', StandardScaler()),
-            ('classifier', models[self.classifier_name][0])
-        ])
-        param_grid = models[self.classifier_name][1]
-        grid_search = GridSearchCV(pipeline, param_grid, cv=5)
-        grid_search.fit(self.X_train, self.y_train)
-
-        return grid_search.best_params_
+        self.dataset_file = st.sidebar.file_uploader('Choose the Dataset', 'csv')
+        self.classifier_name = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'Naive Bayes'))
+        self.n_features = st.sidebar.slider("Number of Features (RFE)", 1, 30, value=10)
 
 
     def load_dataset(self):
@@ -92,16 +54,63 @@ class App:
         st.write(self.dataset.head(10))
         st.write('### Columns')
         st.write(self.dataset.columns.values)
+
+        # clean dataset
         self.dataset.dropna(axis=1, inplace=True)
         self.dataset.drop(['id'], axis=1, inplace=True)
+
         st.write('### Last 10 rows of the dataset')
         st.write(self.dataset.tail(10))
+
         self.dataset['diagnosis'] = self.dataset['diagnosis'].map({'M':1, 'B':0})
         self.X = self.dataset.drop(['diagnosis'], axis=1)
         self.y = self.dataset['diagnosis']
+
         st.pyplot(self.draw_corr().figure)
         plt.clf()
         st.pyplot(self.scatter_plt().figure)
+
+
+    def draw_corr(self):
+        st.write("### Correlation Matrix")
+
+        sns.set_theme(style="white")
+        corr = self.dataset.corr()
+        mask = np.triu(np.ones_like(corr, dtype=bool))
+        f, ax = plt.subplots(figsize=(15, 15))
+        cmap = sns.diverging_palette(230, 20, as_cmap=True)
+
+        corr_matrix_fig = sns.heatmap(corr, cmap=cmap, mask=mask, vmin=corr.values.min(), vmax=1, center=0, annot=True,
+                                square=True, linewidths=.5, cbar_kws={"shrink": .5}, annot_kws={"size": 6.5})
+
+        return corr_matrix_fig
+        
+
+    def scatter_plt(self):
+        st.write("### Scatter Plot")
+        values = list(self.dataset.columns.values)
+        scatter_x = st.selectbox('Select X (default: radius_mean)', values, index=values.index('radius_mean'))
+        scatter_y = st.selectbox('Select Y (default: texture_mean', values, index=values.index('texture_mean'))
+
+        plt.figure(figsize=(5,5))
+        scatter = sns.scatterplot(self.dataset, x=scatter_x, y=scatter_y, hue='diagnosis', palette={1:'red', 0:'green'}, alpha=.5)
+        plt.legend(labels=['Malignant', 'Benign'], loc='upper right')
+
+        return scatter
+
+
+    def preprocess(self):
+        # split data into training and test datasets
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=42)\
+
+        # set up Recursive Feature Elimination to find the best features
+        estimator = SVC(kernel="linear")
+        selector = RFE(estimator, n_features_to_select=self.n_features, step=1)
+        selector = selector.fit(self.X_train, self.y_train)
+        self.selected_features = self.X.columns[selector.support_].values
+
+        st.write("### Selected Features (After RFE)")
+        st.write(self.selected_features)
 
 
     def model_selection(self):
@@ -119,14 +128,25 @@ class App:
             self.model = GaussianNB()
 
 
-    def init_page(self):
-        st.title('YZUP Project')
-        st.write('## Explore Different Classifiers')
-        self.dataset_file = st.sidebar.file_uploader('Choose the Dataset', 'csv')
-        self.classifier_name = st.sidebar.selectbox('Select Classifier', ('KNN', 'SVM', 'Naive Bayes'))
-        self.n_features = st.sidebar.slider("Number of Features (RFE)", 1, 30, value=10)
+    def gridsearch(self):
+        # model parameter grids
+        models = {
+            'KNN': (KNeighborsClassifier(), {'classifier__n_neighbors': (3, 5, 7, 9), 'classifier__leaf_size': (20,40,1), 'classifier__p': (1,2), 'classifier__weights': ('uniform', 'distance'), 'classifier__metric': ('minkowski', 'chebyshev'),}),
+            'SVM': (SVC(), {'classifier__C': (0.1, 1, 10, 15), 'classifier__kernel': ('linear', 'poly', 'rbf', 'sigmoid'), 'classifier__coef0': (0.0, 10.0, 1.0), 'classifier__shrinking': (True, False)})
+        }
+        pipeline = Pipeline([
+            ('scaler', StandardScaler()),
+            ('classifier', models[self.classifier_name][0])
+        ])
+        param_grid = models[self.classifier_name][1]
 
-        
+        # apply gridsearch
+        grid_search = GridSearchCV(pipeline, param_grid, cv=5)
+        grid_search.fit(self.X_train, self.y_train)
+
+        return grid_search.best_params_
+
+
     def evaluate(self):
         y_pred = self.model.predict(self.X_test[self.selected_features])
         scores = {'Accuracy':accuracy_score(self.y_test, y_pred), 
@@ -140,17 +160,27 @@ class App:
 
 
     def train(self):
+        # select model
         self.model_selection()
+        st.write(f'### Classifier: {self.classifier_name}')
         if self.classifier_name != 'Naive Bayes':
             best_params = self.gridsearch()
             self.model.set_params(**best_params)
+            st.write('Best Parameters (GridSearchCV)')
             st.write(best_params)
+
+        # train
         self.model.fit(self.X_train[self.selected_features], self.y_train)
-        st.write(f'### Classifier: {self.classifier_name}')
+
+        # show results
         plt.clf()
         scores, heatmap = self.evaluate()
-        # evaluation = f"Accuracy Score: {scores['Accuracy']} - Precision Score: {scores['Precision']} - Recall Score: {scores['Recall']} - F1 Score: {scores['F1']}"
-        st.write(scores)
+
+        st.write('Test Results')
+        st.write(f"Accuracy Score: :green[{scores['Accuracy']}]")
+        st.write(f"Precision Score: :green[{scores['Precision']}]")
+        st.write(f"Recall Score: :green[{scores['Recall']}]")
+        st.write(f"F1 Score: :green[{scores['F1']}]")
         st.pyplot(heatmap.figure)
 
 
